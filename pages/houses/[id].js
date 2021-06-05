ef6ed0f17a314991c9cc93c2c4b918981341682c
zahid hasan <hzahid2999@gmail.com>
2021-06-05 23:24:47 +0600

    activated modal and redux

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useStoreActions } from "easy-peasy";
 import Head from "next/head";
 import DateRangePicker from "../../components/DateRangePicker";
 import Layout from "../../components/Layout";
@@ -21,6 +22,9 @@ const House = (props) => {
   const [numberOfNightsBetweenDates, setNumberOfNightsBetweenDates] =
     useState(0);
   const [dateChosen, setDateChosen] = useState(false);
+  const setShowLoginModal = useStoreActions(
+    (actions) => actions.modals.setShowLoginModal
+  );
   return (
     <Layout
       content={
@@ -57,7 +61,14 @@ const House = (props) => {
                 <p>
                   ${(numberOfNightsBetweenDates * props.house.price).toFixed(2)}
                 </p>
-                <button className="reserve">Reserve</button>
+                <button
+                  className="reserve"
+                  onClick={() => {
+                    setShowLoginModal();
+                  }}
+                >
+                  Reserve
+                </button>
               </div>
             )}
           </aside>
